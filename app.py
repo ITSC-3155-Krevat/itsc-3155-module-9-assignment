1be587bead8668a32f90ef422b05dbc5cc04ccde
Austin Profenius <pro162@gmail.com>
2023-03-19 22:31:00 -0400

    feature 2

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 from src.repositories.movie_repository import get_movie_repository
 
 app = Flask(__name__)
 
-# Get the movie repository singleton to use throughout the application
 movie_repository = get_movie_repository()
 
 
@@ -15,17 +14,23 @@ def index():
 
 @app.get('/movies')
 def list_all_movies():
-    # TODO: Feature 1
-    return render_template('list_all_movies.html', list_movies_active=True)
+     # TODO: Feature 1
+    movies = movie_repository.get_all_movies()
+    movie_repository.create_movie("Star Wars", "George Lucas", 5)
+    return render_template('list_all_movies.html', movies=movies, list_movies_active=True)
 
 
 @app.get('/movies/new')
 def create_movies_form():
     return render_template('create_movies_form.html', create_rating_active=True)
 
-
-@app.post('/movies')
+@app.route('/movies', methods=['POST', 'GET'])
 def create_movie():
+    if request.method == 'POST':## has something to do with the add task button
+        task_title = request.form['title'] 
+        task_director = request.form['director'] 
+        task_rate = request.form['rate'] 
+        movie_repository.create_movie(task_title,task_director,task_rate)
     # TODO: Feature 2
     # After creating the movie in the database, we redirect to the list all movies page
     return redirect('/movies')
@@ -35,27 +40,3 @@ def create_movie():
 def search_movies():
     # TODO: Feature 3
     return render_template('search_movies.html', search_active=True)
-
-
-@app.get('/movies/<int:movie_id>')
-def get_single_movie(movie_id: int):
-    # TODO: Feature 4
-    return render_template('get_single_movie.html')
-
-
-@app.get('/movies/<int:movie_id>/edit')
-def get_edit_movies_page(movie_id: int):
-    return render_template('edit_movies_form.html')
-
-
-@app.post('/movies/<int:movie_id>')
-def update_movie(movie_id: int):
-    # TODO: Feature 5
-    # After updating the movie in the database, we redirect back to that single movie page
-    return redirect(f'/movies/{movie_id}')
-
-
-@app.post('/movies/<int:movie_id>/delete')
-def delete_movie(movie_id: int):
-    # TODO: Feature 6
-    pass
