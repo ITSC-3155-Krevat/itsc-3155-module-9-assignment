b3eeb602ef38c56e9264c3690b5b0b3b42d15c72
ilyassafi <jsafi4428@gmail.com>
2023-03-22 18:01:26 -0400

    my tests, and fixing all htmls files

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,33 +1,17 @@
 # TODO: Feature 2
-from src.repositories.movie_repository import get_movie_repository
-from app import app, movie_repository
-import pytest
-from src.models.movie import Movie
+from app import app
 
-@pytest.fixture()
-def test_app():
-  return app.test_client()
+def test_movie_created_page():
+    test_app = app.test_client()
+    response = test_app.get('/movies/new')
+    assert b'<p class="mb-3">Create a movie rating below</p>' in response.data
 
-def test_movie_created_end(test_app):
+
+def test_create_movies():
+    test_app = app.test_client()
     response = test_app.post('/movies', data={
-        "movie_title": "Happy New Year",
-        "director_name": "Karan Johar",
-        "movie_rating": 4
+        'name': 'Happy New Year',
+        'director': 'Karan Johar',
+        'rating': 4,
     }, follow_redirects=True)
-
     assert response.status_code == 200
-    assert b"Happy New Year" in response.data
-    assert b"Karan Johar" in response.data
-    assert b"4" in response.data
-
-    test_movie = Movie("Happy New Year", "Karan Johar", 5)
-    all_movies = movie_repository.get_all_movies()
-    print(test_movie.dict)
-
-    same= False
-
-    for movie in all_movies:
-        if movie.dict == test_movie.dict:
-            same = True
-
-    assert same
