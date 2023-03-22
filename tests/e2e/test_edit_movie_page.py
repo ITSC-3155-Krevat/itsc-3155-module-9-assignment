99a112d583dea7c84ab30161ced4173f3ff8e6df
teandra-davis <davisteandra@gmail.com>
2023-03-22 19:18:46 -0400

    Finished e2e testing

--- tests/e2e/test_edit_movie_page.py
+++ tests/e2e/test_edit_movie_page.py
@@ -7,8 +7,27 @@ from flask.testing import FlaskClient
 
 
 def test_edit_page(test_app: FlaskClient):
-    response = test_app.get('/movies/<int:movie_id>/edit')
+    response = test_app.get('/movies/1/edit')
     response_data = response.data.decode('utf-8')
 
     assert '<h1 class="mb-5">Edit Movie</h1>' in response_data
+    assert response.status_code == 200
+
+#def test_update_movie(test_app: FlaskClient):
+   # movie_repo = get_movie_repository()
+   # movie = movie_repo.get_movie_by_id(1)
+
+    
+   # response = test_app.post('/movies/1', data={
+   #         'title': 'Title',
+    #        'director': 'Director',
+     #       'rating': '5'
+      #  }, follow_redirects=True)
+    
+    # assert response.status_code == 200
+    
+    # data = response.data.decode('utf-8')
+    
+    # assert '<h1>Title: Title</h1>' in data
+    
         
