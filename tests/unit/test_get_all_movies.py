a5493e8daee0b67f6e263649075db230699944a7
Neil_Huntley <neilhuntley21@gmail.com>
2023-03-22 18:34:14 -0400

    adding e2e and unit tests for task1

--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,1 +1,24 @@
 # TODO: Feature 1
+from src.repositories.movie_repository import get_movie_repository
+from src.models.movie import Movie
+
+def test_get_all_movies():
+    movie_repository = get_movie_repository()
+    movie_repository.clear_db()
+    assert len(movie_repository.get_all_movies()) == 0
+    movie_repository.create_movie("movie1","John1",1)
+    assert len(movie_repository.get_all_movies()) == 1
+    data = movie_repository.get_all_movies()
+    for id in data:
+        assert movie_repository.get_movie_by_id(id).title == "movie1"
+        assert movie_repository.get_movie_by_id(id).director == "John1"
+        assert movie_repository.get_movie_by_id(id).rating == 1
+    
+    # movie_repository.create_movie("movie2","John2",1)
+    # movie_repository.create_movie("movie3","John3",1)
+    # data = movie_repository.get_all_movies()
+    # for id in data:
+    #     assert id != 0
+    #     assert movie_repository.get_movie_by_id(id) != None
+    #     assert movie_repository.get_movie_by_id(id).rating == 1
+
