7a920d2327c381197d35b7b347aae1ee621bda7f
Evan Huntley <evanhuntley21@gmail.com>
2023-03-22 22:22:37 -0400

    fixed tests

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -3,21 +3,23 @@ from app import app
 
 def test_create_movie(test_app):
 
-    # response = test_app.post('/movies', data={
-    #     'dir_name': 'Jim',
-    #     'movie_name': 'MyMovie',
-    #     'rating': "3"
-    # }, follow_redirects=True)
+    response = test_app.post('/movies', data={
+        'mname': 'Jim',
+        'dir': 'MyMovie',
+        'rating': "3"
+    }, follow_redirects=True)
 
-    # assert response.status_code == 200
-    # response = test_app.get("/movies")
-    # data = response.data.decode('utf-8')
-
-    # assert 'Jim' in data and 'MyMovie' in data and "3" in data
+    assert response.status_code == 200
+    response = test_app.get("/movies")
+    data = response.data.decode('utf-8')
+    print (data)
+    assert 'Jim' in data 
+    assert 'Mymovie' in data 
+    assert "3" in data
 
     response = test_app.post('/movies', data={
-    'dir_name': 'Bob',
-    'movie_name': 'MyMovie1',
+    'dir': 'Bob',
+    'mname': 'Mymovie1',
     'rating': "6"
     }, follow_redirects=True)
     assert response.status_code == 200
@@ -27,8 +29,8 @@ def test_create_movie(test_app):
     assert not('Jim' in data and 'MyMovie1' in data and "6" in data)
 
     response = test_app.post('/movies', data={
-    'dir_name': '',
-    'movie_name': 'MyMovie2',
+    'dir': '',
+    'mname': 'Mymovie2',
     'rating': "5"
     }, follow_redirects=True)
 
@@ -36,14 +38,14 @@ def test_create_movie(test_app):
     response = test_app.get("/movies")
     data = response.data.decode('utf-8')
 
-    assert not( 'MyMovie2' in data and "5" in data)
+    assert not( 'Mymovie2' in data and "5" in data)
 
     response = test_app.post('/movies', data={
-    'dir_name': 'John',
-    'movie_name': '',
+    'dir': 'John',
+    'mname': '',
     'rating': "1"
     }, follow_redirects=True)
-
+    
     assert response.status_code == 200
     response = test_app.get("/movies")
     data = response.data.decode('utf-8')
