a214589866f436bcd4459fd3b3cd6c0969641a45
Gage <gbrown68@uncc.edu>
2023-03-22 21:17:43 -0400

    Completed task 3 and 4
    Completed unit and e2e tests for both tasks 3 and 4.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, abort, request
+from src.models.movie import Movie
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -7,6 +8,11 @@ app = Flask(__name__)
 # Get the movie repository singleton to use throughout the application
 movie_repository = get_movie_repository()
 
+
+#Temp movie
+movie_repository.create_movie('Interstellar', 'Christopher Nolan', 10)
+movie_repository.create_movie('Inception', 'Christopher Nolan', 10)
+movie_repository.create_movie('The Dark Knight', 'Christopher Nolan', 9)
 
 @app.get('/')
 def index():
@@ -31,17 +37,36 @@ def create_movie():
     return redirect('/movies')
 
 
-@app.get('/movies/search')
+@app.route('/movies/search')
 def search_movies():
-    # TODO: Feature 3
-    return render_template('search_movies.html', search_active=True)
+    
+    # Get the movie title from the query string
+    title = request.args.get('title')
+    if title: 
+        title = title.title()
+
+    # If no title is provided, just render the search page
+    if not title:
+        return render_template('search_movies.html')
+
+    # Check if the movie exists in the repository
+    movie = movie_repository.get_movie_by_title(title)
+
+    if movie:
+        # If the movie exists, get its rating and render the search page with the result
+        rating = movie.rating
+        return render_template('search_movies.html', search_active=True, title=title, rating=rating)
+    else:
+        # If the movie doesn't exist, render the search page with an error message
+        return render_template('search_movies.html', search_active=True, title=title, rating=None)
 
 
 @app.get('/movies/<int:movie_id>')
 def get_single_movie(movie_id: int):
-    # TODO: Feature 4
-    return render_template('get_single_movie.html')
-
+    # Feature 4 #
+    
+    movie = movie_repository.get_movie_by_id(movie_id)
+    return render_template('get_single_movie.html', movie=movie, movie_repository=movie_repository)
 
 @app.get('/movies/<int:movie_id>/edit')
 def get_edit_movies_page(movie_id: int):
@@ -59,3 +84,8 @@ def update_movie(movie_id: int):
 def delete_movie(movie_id: int):
     # TODO: Feature 6
     pass
+
+
+if __name__ == "__main__":
+    
+    app.run(debug=True)
--- tests/e2e/test_view_movie_page.py
+++ tests/e2e/test_view_movie_page.py
@@ -1,1 +1,51 @@
-# TODO: Feature 4
+
+# Feature 4 E2E Testing
+
+
+from src.repositories.movie_repository import get_movie_repository
+from app import app
+
+# Create a test movie and get its id for testing
+movie_repository = get_movie_repository()
+test_movie = movie_repository.create_movie('Interstellar', 'Christopher Nolan', 5)
+test_movie_id = test_movie.movie_id
+
+def test_get_movie_by_id():
+    # Retrieve the test movie by id
+    movie = movie_repository.get_movie_by_id(test_movie_id)
+
+    test_app = app.test_client()
+    page = test_app.get(f'/movies/{ test_movie_id }')
+    page_data = response_data = page.data.decode('utf-8')
+    
+    assert '<h2>No movie in this database with that id.</h2>' not in page_data
+    # Problem test
+    #assert b'Christopher Nolan' in page.data
+
+    assert page.status_code == 200
+
+    assert b'Interstellar' in page.data
+
+    assert b'5' in page.data
+
+    assert b'Delete' in page.data
+    
+    assert b'Edit' in page.data
+
+    # Delete the test movie after testing is complete
+    movie_repository.delete_movie(test_movie_id)
+
+
+def test_get_movie_by_id_empty_repository():
+    # Given an empty movie repository
+    movie_repository.clear_db()
+
+    # When we attempt to retrieve a movie by an ID
+    test_app = app.test_client()
+    page = test_app.get('/movies/1')
+    page_data = page.data.decode('utf-8')
+
+    # Then the movie should not be found
+    assert '<h2>No movie in this database with that id.</h2>' in page_data
+
+
