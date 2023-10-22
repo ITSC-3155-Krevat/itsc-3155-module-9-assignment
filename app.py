5d01665efee0597f113e62647b43e657a14a92f3
Zane Hutchens <zhut654@gmail.com>
2023-10-22 15:43:02 -0400

    Finished code to create movie

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, abort
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -27,6 +27,12 @@ def create_movies_form():
 @app.post('/movies')
 def create_movie():
     # TODO: Feature 2
+    movie_rating = request.form.get('rating')
+    movie_name = request.form.get('name')
+    movie_director = request.form.get('director')
+    if movie_rating == None or movie_name.strip() == '' or movie_director.strip() == '':
+        abort(400, "Entered bad information into form") 
+    movie_repository.create_movie(title=movie_name, director=movie_director, rating=movie_rating)
     # After creating the movie in the database, we redirect to the list all movies page
     return redirect('/movies')
 
