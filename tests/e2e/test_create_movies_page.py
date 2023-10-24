927450c5a10050fb6307fbbf6e4740b299b9c7be
Zane Hutchens <zhut654@gmail.com>
2023-10-23 21:43:34 -0400

    Finished end to end test

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,5 +1,42 @@
 # TODO: Feature 2
-from app import app
-import pytest
+from flask.testing import FlaskClient
 
+def test_post_movie(test_app: FlaskClient) -> None: 
+    response = test_app.post('/movies', data={
+        'rating': '2',
+        'name': 'iron man',
+        'director': 'zane'
+    }, follow_redirects = True)
+
+    data = response.data.decode()
+    
+    assert '<td>2' in data 
+    assert '<td> iron man' in data
+    assert '<td> zane' in data
+    assert response.status_code == 302
+
+def test_post_movie_bad_information(test_app: FlaskClient) -> None:
+    response = test_app.post('/movies', data={
+        'rating': '2',
+        'name': 'iron man',
+        'director': ''
+    }, follow_redirects = True)
+
+    assert response.status_code == 400
+
+    response = test_app.post('/movies', data={
+        'rating': None,
+        'name': 'iron man',
+        'director': 'zane'
+    }, follow_redirects = True)
+
+    assert response.status_code == 400
+
+    response = test_app.post('/movies', data={
+        'rating': '3',
+        'name': '',
+        'director': 'zane'
+    }, follow_redirects = True)
+
+    assert response.status_code == 400
 
