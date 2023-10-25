e77a27034e7de4931093d4e464cc2b42ccd9e046
Riley_Retzloff <rirye811@gmail.com>
2023-10-25 19:19:22 -0400

    Completed unit test, removed unnecessary conftest.py file, and restructured imports and fixtures in both e2e and unit test files.

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,5 +1,15 @@
 # TODO: Feature 1
 
+from src.repositories.movie_repository import get_movie_repository
+import pytest
+
+# Loading movies into a test repository
+@pytest.fixture(scope='module')
+def preload_movies():
+    repo = get_movie_repository()
+    repo.create_movie('Call Me By Your Name', 'Luca Guadagnino', 5)
+    repo.create_movie('Moonlight', 'Barry Jenkins', 5)
+
 def test_all_movies_page(test_app, preload_movies):
     # Getting response from test app for list_all_movies page
     response = test_app.get('/movies')
--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,2 +1,21 @@
 # TODO: Feature 1
 
+import pytest
+from app import app, list_all_movies
+from unittest.mock import patch
+
+def test_list_all_movies():
+    with patch('app.render_template') as mock_render_template:
+        # Mocking the get_all_movies method from movie_repository
+        with patch('app.movie_repository.get_all_movies') as mock_get_all_movies:
+            # Setting the return value for mock_get_all_movies
+            mock_get_all_movies.return_value = [
+                {'title': 'Call Me By Your Name', 'director': 'Luca Guadagnino', 'rating': 5},
+                {'title': 'Moonlight', 'director': 'Barry Jenkins', 'rating': 5}
+            ]
+
+            # Call the list_all_movies function
+            list_all_movies()
+
+            # Assert that render_template was called correctly
+            mock_render_template.assert_called_once_with('list_all_movies.html', movies=mock_get_all_movies.return_value, list_movies_active=True)
