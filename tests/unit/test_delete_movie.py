c2dfacb5a57712abdddcfbf602b12b1adc7c4110
Safa Rasheed <safarasheed02@gmail.com>
2023-10-26 19:23:21 -0400

    Added unit tests for deleting movie

--- tests/unit/test_delete_movie.py
+++ tests/unit/test_delete_movie.py
@@ -1,1 +1,27 @@
 # TODO: Feature 6
+from app import app, movie_repository
+import pytest
+
+
+def test_movie_id_not_found():
+    with app.test_client() as client:
+        movie = movie_repository.create_movie('Barbie','Greta Gerwig',5)
+        movie_repository.delete_movie(movie.movie_id)
+
+        response = client.get('/movies/<int:movie_id>/delete')
+
+    assert response.status_code == 404
+    
+
+
+def test_delete_existing_movie():
+    movie = movie_repository.create_movie('Barbie','Greta Gerwig',5)
+    movie_repository.delete_movie(movie.movie_id) 
+    movies = movie_repository.get_all_movies()
+
+    assert len(movies) == 0 
+
+
+
+
+        
