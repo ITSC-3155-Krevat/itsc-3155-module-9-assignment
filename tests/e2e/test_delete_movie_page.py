7e723683ecf0b530ccc96fc4558bf6d5ed39278e
Safa Rasheed <safarasheed02@gmail.com>
2023-10-27 09:14:32 -0400

    final edit of e2e

--- tests/e2e/test_delete_movie_page.py
+++ tests/e2e/test_delete_movie_page.py
@@ -5,7 +5,6 @@ from app import app, movie_repository
 def test_movie_deleted(test_app):
     movie_repository.clear_db()
 
-
     movie_repository.create_movie('Barbie','Greta Gerwig',5)
     movie2 = movie_repository.create_movie('Red, White and Royal Blue','Matthew Lopez',5)
 
