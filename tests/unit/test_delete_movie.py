33379c88061cf4787819469458db6da6a85dc71b
Safa Rasheed <safarasheed02@gmail.com>
2023-10-26 23:41:10 -0400

    Edited unit tests, and added e2e tests

--- tests/unit/test_delete_movie.py
+++ tests/unit/test_delete_movie.py
@@ -4,6 +4,7 @@ import pytest
 
 
 def test_movie_id_not_found():
+    movie_repository.clear_db()
     with app.test_client() as client:
         movie = movie_repository.create_movie('Barbie','Greta Gerwig',5)
         movie_repository.delete_movie(movie.movie_id)
@@ -11,15 +12,17 @@ def test_movie_id_not_found():
         response = client.get('/movies/<int:movie_id>/delete')
 
     assert response.status_code == 404
-    
+
 
 
 def test_delete_existing_movie():
+    movie_repository.clear_db()
     movie = movie_repository.create_movie('Barbie','Greta Gerwig',5)
     movie_repository.delete_movie(movie.movie_id) 
     movies = movie_repository.get_all_movies()
 
     assert len(movies) == 0 
+ 
 
 
 
