edcf88baea8102dfbd4af628247fb8596d0d643f
Ravi Patel <patelravi7703@gmail.com>
2023-10-27 16:30:26 -0400

    feture 2 update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, abort
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -7,8 +7,10 @@ app = Flask(__name__)
 # Get the movie repository singleton to use throughout the application
 movie_repository = get_movie_repository()
 
-# movie_repository.create_movie("Dostana", "Ravi", 3)
-# movie_repository.create_movie("dhoom", "abhikesh", 5)
+movie_repository = {
+    1: {"title": "Movie 1", "director": "Director 1", "rating": 4.5},
+    2: {"title": "Movie 2", "director": "Director 2", "rating": 3.8}
+}
 
 
 @app.get('/')
@@ -43,16 +45,43 @@ def search_movies():
 @app.get('/movies/<int:movie_id>')
 def get_single_movie(movie_id: int):
     # TODO: Feature 4
-    # return render_template('get_single_movie.html')
-    
-    movie = movie_repository.get_movie_by_id(movie_id)
-    if movie is None:
-        return render_template('get_single_movie.html')
-    return render_template('get_single_movie.html', movie=movie)
+    single_movie = movie_repository.get_movie_by_id(movie_id)
+    if single_movie is None:
+        abort(400)
+
+    return render_template('get_single_movie.html', single_movie = single_movie, movie_id = movie_id)
+
 
 @app.get('/movies/<int:movie_id>/edit')
 def get_edit_movies_page(movie_id: int):
-    return render_template('edit_movies_form.html')
+    global movie_repository
+    movie_to_edit = movie_repository.get_movie_by_id(movie_id)
+    if movie_to_edit is None:
+        abort(404)
+    return render_template('edit_movies_form.html', movie=movie_to_edit)
+
+# @app.get('/movies/<int:movie_id>')
+# def get_single_movie(movie_id: int):
+#     # Fetch the movie from the repository using its ID
+#     single_movie = movie_repository.get(movie_id)
+
+#     if single_movie:
+#         return render_template('get_single_movie.html', single_movie=single_movie, movie_id=movie_id)
+#     else:
+#         abort(404)
+
+# # Route to edit a movie
+# @app.get('/movies/<int:movie_id>/edit')
+# def get_edit_movies_page(movie_id: int):
+#     movie_to_edit = movie_repository.get(movie_id)
+
+#     if movie_to_edit:
+#         return render_template('edit_movies_form.html', movie=movie_to_edit)
+#     else:
+#         abort(404)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 
 @app.post('/movies/<int:movie_id>')
--- tests/e2e/test_view_movie_page.py
+++ tests/e2e/test_view_movie_page.py
@@ -2,17 +2,25 @@
 
 from src.models.movie import Movie
 from src.repositories.movie_repository import get_movie_repository
+from flask import url_for
+# import pytest
+from app import app 
+
+# @pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
 def test_view_movie_page(client):
     repo = get_movie_repository()
-    movie = Movie(1, 'Movie1', 'Director1', 5)
+    movie = Movie(1, 'Golmal', 'Rohit Shetty', 5)
     repo._db = {1: movie}
-    
-    response = client.get('/movies/1')
+
+    response = client.get(url_for('get_single_movie', movie_id=1))
     assert response.status_code == 200
-    assert 'Movie Details' in response.data.decode('utf-8')
-    assert 'Movie1' in response.data.decode('utf-8')
-    
-    response = client.get('/movies/2')
-    assert response.status_code == 404
+    assert movie.title in str(response.data) 
+
+    response = client.get(url_for('get_single_movie', movie_id=2))
+    assert response.status_code == 400
     assert 'Movie not found' in response.data.decode('utf-8')
