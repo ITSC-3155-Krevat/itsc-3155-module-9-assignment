a892dd11e768ad4567455f4050578e6d3cdcc235
Landon Nalewaja <landon.nalewaja@gmail.com>
2024-03-18 15:07:49 -0400

    Adding Feature 4 Capabilities

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 # Get the movie repository singleton to use throughout the application
 movie_repository = get_movie_repository()
 
-
 @app.get('/')
 def index():
     return render_template('index.html')
@@ -40,7 +39,11 @@ def search_movies():
 @app.get('/movies/<int:movie_id>')
 def get_single_movie(movie_id: int):
     # TODO: Feature 4
-    return render_template('get_single_movie.html')
+    # when movie in movie page is clicked, get request is made
+    # this sends the movie_id
+    
+    movie = movie_repository.get_movie_by_id(movie_id)
+    return render_template('get_single_movie.html', movie=movie)
 
 
 @app.get('/movies/<int:movie_id>/edit')
