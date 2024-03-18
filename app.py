6edf55b38f9c9d98174e5b7cf1c5b3e2abded59c
Ronni Elhadidy <eronni1@gmail.com>
2024-03-18 16:56:33 -0400

    feature 5 commits

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 # Get the movie repository singleton to use throughout the application
 movie_repository = get_movie_repository()
-
+movie_repository.create_movie('Spiderman', 'Ronni Elhadidy', 5)
+movie = movie_repository.get_movie_by_title('Spiderman')
+print(movie.movie_id)
+if __name__ == "__main__":
+    app.run(debug=True)
 
 @app.get('/')
 def index():
@@ -45,13 +49,30 @@ def get_single_movie(movie_id: int):
 
 @app.get('/movies/<int:movie_id>/edit')
 def get_edit_movies_page(movie_id: int):
-    return render_template('edit_movies_form.html')
+   
+    movie = movie_repository.get_movie_by_id(movie_id)
+    
+    
+    if not movie:
+        return "Movie not found", 404
+    
+    
+    return render_template('edit_movies_form.html', movie=movie)
 
 
 @app.post('/movies/<int:movie_id>')
 def update_movie(movie_id: int):
-    # TODO: Feature 5
-    # After updating the movie in the database, we redirect back to that single movie page
+    title = request.form['title']
+    director = request.form['director']
+    rating = request.form.get('rating', type=int)
+    
+    updated_movie = {
+        'title': title,
+        'director': director,
+        'rating': rating
+    }
+    
+    movie_repository.update_movie(movie_id, updated_movie)
     return redirect(f'/movies/{movie_id}')
 
 
