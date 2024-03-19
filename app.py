af607261b1b3d88fe98b63b9716b9545592b6126
Kendall_Tart <kenbazonct14@gmail.com>
2024-03-19 00:11:24 -0400

    Feature 1 conflict fix

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 # Get the movie repository singleton to use throughout the application
 movie_repository = get_movie_repository()
 
-@app.get("/")
+@app.get('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-@app.get("/movies")
+@app.get('/movies')
 def list_all_movies():
     movie_repo = get_movie_repository()
     all_movies = movie_repo.get_all_movies()
@@ -22,12 +22,12 @@ def list_all_movies():
         for movie_id, movie in all_movies.items()
     ]
 
-    return render_template("list_all_movies.html", list_movies_active=True, movies=movies_list)
+    return render_template('list_all_movies.html', list_movies_active=True, movies=movies_list)
 
 
-@app.get("/movies/new")
+@app.get('/movies/new')
 def create_movies_form():
-    return render_template("create_movies_form.html", create_rating_active=True)
+    return render_template('create_movies_form.html', create_rating_active=True)
 
 
 @app.post('/movies')
@@ -36,31 +36,31 @@ def create_movie():
     return redirect('/movies')
 
 
-@app.get("/movies/search")
+@app.get('/movies/search')
 def search_movies():
     # TODO: Feature 3
-    return render_template("search_movies.html", search_active=True)
+    return render_template('search_movies.html', search_active=True)
 
 
-@app.get("/movies/<int:movie_id>")
+@app.get('/movies/<int:movie_id>')
 def get_single_movie(movie_id: int):
     # TODO: Feature 4
-    return render_template("get_single_movie.html")
+    return render_template('get_single_movie.html')
 
 
-@app.get("/movies/<int:movie_id>/edit")
+@app.get('/movies/<int:movie_id>/edit')
 def get_edit_movies_page(movie_id: int):
-    return render_template("edit_movies_form.html")
+    return render_template('edit_movies_form.html')
 
 
-@app.post("/movies/<int:movie_id>")
+@app.post('/movies/<int:movie_id>')
 def update_movie(movie_id: int):
     # TODO: Feature 5
     # After updating the movie in the database, we redirect back to that single movie page
-    return redirect(f"/movies/{movie_id}")
+    return redirect(f'/movies/{movie_id}')
 
 
-@app.post("/movies/<int:movie_id>/delete")
+@app.post('/movies/<int:movie_id>/delete')
 def delete_movie(movie_id: int):
     # TODO: Feature 6
     pass
