1ad60413a0eeeb58420f68b415dbe6e2de6cd3ba
Brandon Russell <brandonerussell8@gmail.com>
2024-03-18 20:16:20 -0400

    Revised Testing. Hopefully Better

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,51 +1,45 @@
 # TODO: Feature 1
-import pytest
-from flask.testing import FlaskClient
+
+from src.models.movie import Movie
 from app import app
 from src.repositories.movie_repository import get_movie_repository
 
-@pytest.fixture
-def client():
-    app.config['TESTING'] = True
-
-    with app.test_client() as client:
-        yield client
-
-
-def test_list_all_movies_page(client: FlaskClient):
+def test_list_all_movies_page(test_app):
     #test to see if the page is working
 
-    response = client.get('/movies')
-    response_data = response.data.decode('utf-8')
-
-    #assert
+    response = test_app.get('/movies')
     assert response.status_code == 200
     assert response.status_code != 404
 
+    response_data = response.data.decode('utf-8')
+
     assert b'<h1 class="mb-5">All Movies</h1>' in response.data
     assert b'<p class="mb-3">See our list of movie ratings below</p>' in response.data
+   
 
-def test_list_all_movies_working_table(client: FlaskClient):
+def test_list_all_movies_working_table(test_app):
     #test to see if the table is working and displaying the movies
-
     movie_repository = get_movie_repository()
     movie_repository.clear_db()
-
 
     movie_repository.create_movie('The Shawshank Redemption', 'Frank Darabont', 9.2)
     movie_repository.create_movie('The Godfather', 'Francis Ford Coppola', 9.1)
 
-    response = client.get('/movies')
+    response = test_app.get('/movies')
 
     #assert
     assert response.status_code == 200
-    
     assert b'The Shawshank Redemption' in response.data
     assert b'The Godfather' in response.data
     assert b'Frank Darabont' in response.data
     assert b'Francis Ford Coppola' in response.data
     assert b'9.2' in response.data
     assert b'9.1' in response.data
+
+   
+
+
+
 
 
 
--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -4,39 +4,34 @@ from src.repositories.movie_repository import get_movie_repository
 from src.models.movie import Movie
 from app import app
 
-def test_list_all_movies_page():
-    #test to see if the page is working
-
-    with app.test_client() as client:
-        response = client.get('/movies')
-
-    #assert
-    assert response.status_code == 200
-    assert response.status_code != 404
-
-    assert b'<h1 class="mb-5">All Movies</h1>' in response.data
-    assert b'<p class="mb-3">See our list of movie ratings below</p>' in response.data
-
-def test_list_all_movies_working_table():
-    #test to see if the table is working and displaying the movies
-
+#Add the movie to the database and then check if it is there
+def test_get_all_movies():
     movie_repository = get_movie_repository()
     movie_repository.clear_db()
-
     movie_repository.create_movie('The Shawshank Redemption', 'Frank Darabont', 9.2)
     movie_repository.create_movie('The Godfather', 'Francis Ford Coppola', 9.1)
 
-    with app.test_client() as client:
-        response = client.get('/movies')
+    movies = movie_repository.get_all_movies()
 
     #assert
-    assert response.status_code == 200
+    #checking if the 'movies' variale is a dictionary. As the get_all_movies() function returns a dictionary.
+    assert isinstance(movies, dict)
+    #checking if the length of the dictionary is 2. As we added 2 movies to the database
+    assert len(movies) == 2
+
+#check if the database is empty
+def test_empty_db():
+    movie_repository = get_movie_repository()
+    movie_repository.clear_db()
+    movies = movie_repository.get_all_movies()
     
-    assert b'The Shawshank Redemption' in response.data
-    assert b'The Godfather' in response.data
-    assert b'Frank Darabont' in response.data
-    assert b'Francis Ford Coppola' in response.data
-    assert b'9.2' in response.data
-    assert b'9.1' in response.data
+    #assert
+    assert movie_repository.get_all_movies() == {}
+    assert len(movies) == 0
+
+
+
+
+
 
    
