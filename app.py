df132548b9d6c4b73ad0bb990494b776ec89d1a9
Anessa Leng <ane.leng955@gmail.com>
2024-03-20 17:33:36 -0400

    Updating to resolve conflicts and merge

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from src.repositories.movie_repository import get_movie_repository
 
 app = Flask(__name__)
 
-# Get the movie repository singleton to use throughout the application
-movie_repository = get_movie_repository()
+movies = []
+next_movie_id = 1
 
 @app.get('/')
 def index():
@@ -28,11 +28,39 @@ def create_movie():
     # After creating the movie in the database, we redirect to the list all movies page
     return redirect('/movies')
 
+# create/ save feature cindy
+@app.post('/movies/new')
+def create_movie():
+    global next_movie_id
 
-@app.get('/movies/search')
+    title = request.form.get('title')
+    director = request.form.get('director')
+    rating = request.form.get('rating')
+
+    # Check if the movie already exists -- test
+    if any(movie['title'] == title and movie['director'] == director for movie in movies):
+        error_message = "Movie already exists!"
+        return render_template('create_movies_form.html', create_rating_active=True, error=error_message)
+
+    # Create a new movie
+    movie_id = next_movie_id
+    next_movie_id += 1
+    new_movie = {'movie_id': movie_id, 'title': title, 'director': director, 'rating': rating}
+    movies.append(new_movie)
+
+    return redirect('/movies')
+
+
+# Varsha's search movie function
+@app.post('/movies/search')
 def search_movies():
-    # TODO: Feature 3
-    return render_template('search_movies.html', search_active=True)
+    title = request.form.get('title')
+    movies_found = [movie for movie in movies if movie['title'] == title]
+    if movies_found:
+        return render_template('search_movies.html', movies_found=movies_found, search_active=True)
+    else:
+        return render_template('search_movies.html', not_found=True, search_active=True)
+
 
 #Anessa's get single movie feature
 @app.get('/movies/<int:movie_id>')
@@ -44,13 +72,12 @@ def get_single_movie(movie_id: int):
     response = f"Movie with id: {movie_id} not found."
     status_code = 400
     return render_template('get_single_movie.html', movies=False, error_status=status_code, error_message=response)
-    
 
 
 @app.get('/movies/<int:movie_id>/edit')
 def get_edit_movies_page(movie_id: int):
-    return render_template('edit_movies_form.html')
-
+    movie = movie_repository.get_movie_by_id(movie_id)
+    return render_template('edit_movies_form.html', movie=movie)
 
 @app.post('/movies/<int:movie_id>')
 def update_movie(movie_id: int):
