b1fa1cb632ddc48beb5e708acf68a122334a0f18
Cindy Nguyen <114771367+cinguu@users.noreply.github.com>
2024-03-20 19:25:08 -0400

    Update test_create_movie.py
    reupload e2e testing

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,7 +1,7 @@
 # TODO: Feature 2
 
-# test_create_movie.py -- Cindy's Save feature TEST
-from app import app 
+# tests/e2e/test_create_movies_page.py -- Cindy's Save function TEST 
+from app import app
 import pytest
 
 @pytest.fixture
@@ -10,19 +10,19 @@ def client():
         yield client
 
 def test_create_movie_duplicate(client):
-    #add a movie
+    #add a movie 
     client.post('/movies/new', data={
         'title': 'Existing Movie',
         'director': 'Existing Director',
         'rating': '4'
     })
 
-    #create a duplicit
+    #create a duplicit 
     response = client.post('/movies/new', data={
         'title': 'Existing Movie',
         'director': 'Existing Director',
         'rating': '5'
-    }, follow_redirects=True)
+    }, follow_redirects=True)  
 
     assert response.status_code == 200
 
