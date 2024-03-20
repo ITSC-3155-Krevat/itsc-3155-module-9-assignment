bd7f36db9a0275fe26a29a265aaca585c8654e21
Samuel <ssanto6@gmail.com>
2024-03-20 19:53:17 -0400

    Sam's commit of Sams-Branch-1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -28,6 +28,16 @@ def create_movies_form():
 def create_movie():
     # TODO: Feature 2
     # After creating the movie in the database, we redirect to the list all movies page
+    name = request.form.get('name')
+    if name is None:
+        return redirect('/movies/new')
+    director = request.form.get('director')
+    if director is None:
+        return redirect('/movies/new')
+    rating = request.form.get('rating')
+    if rating is None:
+        return redirect('/movies/new')
+    create_movie(name, director, rating)
     return redirect('/movies')
 
 
