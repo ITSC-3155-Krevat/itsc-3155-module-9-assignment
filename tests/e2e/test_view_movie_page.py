fae23bff0341601d59cfe9267186555729c68e80
Anessa Leng <ane.leng955@gmail.com>
2024-03-20 02:27:04 -0400

    Adding tests for feature 4

--- tests/e2e/test_view_movie_page.py
+++ tests/e2e/test_view_movie_page.py
@@ -1,1 +1,50 @@
-# TODO: Feature 4
+#Anessa's get_single_movie test e2e feature
+# Feature 4
+
+import pytest
+from app import app, movie_repository
+
+@pytest.fixture(scope='module')
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_view_movie_page(client):
+    movie_repository.clear_db()
+    movie_id = 9999
+    response = client.get(f'/movies/{movie_id}', data = {
+        'movie_id': movie_id, 
+        'title': 'Parasite', 
+        'director': 'Bong Joon-ho', 
+        'rating': 5
+    }, follow_redirects=True)
+    response_data = response.data.decode('utf-8')
+    assert response.status_code == 200
+    assert '<h2 name="title">{{ movie.title }}</h2>' in response_data
+
+def test_get_single_movie_without_id(client):
+    movie_repository.clear_db()
+    response = client.get('/get_single_movie', data={
+        'title': 'Parasite',
+        'director': 'Bong Joon-ho',
+        'rating': 5
+    }, follow_redirects=True)
+    assert response.status_code == 404
+
+def test_view_movie_page_validation_error(client):
+    movie_repository.clear_db()
+    movie_id = 9999
+    response = client.get(f'/movies/{movie_id}')
+    assert response.status_code != 404
+
+def test_movie_id_not_found(client):
+    movie_repository.clear_db()
+    movie_id = 9999
+    response = client.get(f'/movies/{movie_id}', data = {
+        'title': 'Parasite',
+        'director': 'Bong Joon-ho',
+        'rating': 5
+    }, follow_redirects=True)
+    response_data = response.data.decode('utf-8')
+    assert response.status_code == 200
+    assert '<h1>{{ error_status }} Error</h1>' not in response_data
--- tests/unit/test_get_movie_by_id.py
+++ tests/unit/test_get_movie_by_id.py
@@ -1,1 +1,25 @@
-# TODO: Feature 4
+#Anessa's get_single_movie test unit feature
+# Feature 4
+
+import pytest
+from app import app, movie_repository
+from src.models.movie import Movie
+
+@pytest.fixture(scope='module')
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_get_movie_by_id(client):
+    movie_repository.clear_db()
+    movie = Movie(123, 'Star Wars', 'George Lucas', 5)
+    response = client.get(f'/movies/{movie.movie_id}', follow_redirects=True)
+    assert movie.movie_id == 123
+    assert response.status_code == 200
+
+
+def test_movie_id_validation_error(client):
+    movie_repository.clear_db()
+    movie_id = 9999
+    response = client.get(f'/movies/{movie_id}')
+    assert response.status_code != 404
