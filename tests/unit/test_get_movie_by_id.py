22bbe19456fb8b723096217de5b4ff5640d701f5
Ty Nixon <kaptainnexus@gmail.com>
2024-03-20 13:40:08 -0400

    Update test_get_movie_by_id.py
    Added unit test for feature 4

--- tests/unit/test_get_movie_by_id.py
+++ tests/unit/test_get_movie_by_id.py
@@ -1,1 +1,7 @@
-# TODO: Feature 4
+def test_get_single_movie(movie_repository):
+    # Assuming you have a movie already in the repository with ID 1
+    movie = movie_repository.get_movie_by_id(1)
+    assert movie is not None
+    assert movie.title == 'Iron Man'
+    assert movie.director == 'Jon Favreau'
+    assert movie.rating == 5 
