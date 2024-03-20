9d0f2a9e7d157693e510eb20d77cd49b0b04a127
D'Andre <dtmcfadden99@gmail.com>
2024-03-19 22:18:09 -0400

    part-2 initial

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,1 +1,16 @@
+import pytest
+from app import app
+
 # TODO: Feature 2
+def test_create_movies_page(test_app):
+    client = test_app
+    
+    response = client.post('/movies', data={'title': 'Test title', 'director': 'Director Test', 'rating': 3})
+    
+    assert response.status_code == 302
+    assert response.headers['Location'] == 'http://localhost/movies'
+    
+    assert b'Movie successfully created' in response.data
+    assert b'Test title' in response.data
+    assert b'Director Test' in response.data
+    assert b'3' in response.data
--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,22 @@
 # TODO: Feature 2
+from src.repositories.movie_repository import get_movie_repository
+from src.models.movie import Movie
+
+def test_create_movie():
+    movie_repository = get_movie_repository()
+    movie_repository.clear_db()
+    
+    movie_repository.create_movie('The Shawshank Redemption', 'Frank Darabont', 9.2)
+    
+    movies = movie_repository.get_all_movies()
+    
+    assert isinstance(movies, dict)
+    
+    added_movie = movies.get(1)
+    
+    print("Movies in dictionary:", movies)
+    assert added_movie is not None, "Movie not in dictionary"
+    
+    assert added_movie.title == 'The Shawshank Redemption'
+    assert added_movie.director == 'Frank Darabont'
+    assert added_movie.rating == 9.2
