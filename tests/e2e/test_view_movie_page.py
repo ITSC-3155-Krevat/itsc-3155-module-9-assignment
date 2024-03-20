b3ab752013228a1c8a4dd59da3fb90d504ad9862
Anessa Leng <ane.leng955@gmail.com>
2024-03-20 15:14:13 -0400

    Updating tests for feature 4

--- tests/e2e/test_view_movie_page.py
+++ tests/e2e/test_view_movie_page.py
@@ -3,6 +3,7 @@
 
 import pytest
 from app import app, movie_repository
+from src.models.movie import Movie
 
 @pytest.fixture(scope='module')
 def client():
@@ -10,17 +11,19 @@ def client():
         yield client
 
 def test_view_movie_page(client):
-    movie_repository.clear_db()
-    movie_id = 9999
-    response = client.get(f'/movies/{movie_id}', data = {
-        'movie_id': movie_id, 
-        'title': 'Parasite', 
-        'director': 'Bong Joon-ho', 
-        'rating': 5
+    movie_repository = Movie(9999, 'Parasite', 'Bong Joon-ho', 5)
+    assert movie_repository.movie_id == 9999
+    response = client.get(f'/movies/{movie_repository.movie_id}', data = {
+        'title': movie_repository.title,
+        'director': movie_repository.director,
+        'rating': movie_repository.rating
     }, follow_redirects=True)
     response_data = response.data.decode('utf-8')
-    assert response.status_code == 200
-    assert '<h2 name="title">{{ movie.title }}</h2>' in response_data
+    if f'<h4>f"Movie with id: {movie_repository.movie_id} not found."</h4>' in response_data:
+        assert '<h4>{{ error_message }}</h4>' in response_data
+    else:
+        assert response.status_code == 200
+        assert f'<h4>f"Movie with id: {movie_repository.movie_id} not found."</h4>' not in response_data
 
 def test_get_single_movie_without_id(client):
     movie_repository.clear_db()
@@ -36,15 +39,3 @@ def test_view_movie_page_validation_error(client):
     movie_id = 9999
     response = client.get(f'/movies/{movie_id}')
     assert response.status_code != 404
-
-def test_movie_id_not_found(client):
-    movie_repository.clear_db()
-    movie_id = 9999
-    response = client.get(f'/movies/{movie_id}', data = {
-        'title': 'Parasite',
-        'director': 'Bong Joon-ho',
-        'rating': 5
-    }, follow_redirects=True)
-    response_data = response.data.decode('utf-8')
-    assert response.status_code == 200
-    assert '<h1>{{ error_status }} Error</h1>' not in response_data
--- tests/unit/test_get_movie_by_id.py
+++ tests/unit/test_get_movie_by_id.py
@@ -2,6 +2,7 @@
 # Feature 4
 
 import pytest
+from flask import redirect
 from app import app, movie_repository
 from src.models.movie import Movie
 
@@ -10,16 +11,22 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_get_movie_by_id(client):
-    movie_repository.clear_db()
-    movie = Movie(123, 'Star Wars', 'George Lucas', 5)
-    response = client.get(f'/movies/{movie.movie_id}', follow_redirects=True)
-    assert movie.movie_id == 123
-    assert response.status_code == 200
-
+def test_get_movie(client):
+    movie_repository = Movie(123, 'Star Wars', 'George Lucas', 5)
+    assert movie_repository.movie_id == 123
+    response = client.get(f'/movies/{movie_repository.movie_id}', data = {
+        'title': movie_repository.title,
+        'director': movie_repository.director,
+        'rating': movie_repository.rating
+    }, follow_redirects=True)
+    response_data = response.data.decode('utf-8')
+    if response.status_code != 200:
+        assert f'<h4>f"Movie with id: {movie_repository.movie_id} not found."</h4>' in response_data
+        assert '<h2 name="title">Star Wars</h2>' not in response_data
+    elif response.status_code == 200:
+        assert f'<h4>f"Movie with id: {movie_repository.movie_id} not found."</h4>' not in response_data
 
 def test_movie_id_validation_error(client):
-    movie_repository.clear_db()
     movie_id = 9999
     response = client.get(f'/movies/{movie_id}')
     assert response.status_code != 404
