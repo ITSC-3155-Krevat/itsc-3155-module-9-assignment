32f3a8a27b633118921e217bab7bb1f805786c95
Ronni Elhadidy <eronni1@gmail.com>
2024-03-19 23:28:51 -0400

    Unit test done still working on e2e

--- tests/e2e/test_edit_movie_page.py
+++ tests/e2e/test_edit_movie_page.py
@@ -1,1 +1,12 @@
 # TODO: Feature 5
+from flask.testing import FlaskClient
+from src.models.movie import Movie
+
+
+
+def test_edit_page(test_app: FlaskClient):
+    movie = Movie(123, 'Star Wars', 'George Lucas', 5)
+    response = test_app.get(f"/movies/{movie.movie_id}/edit")  
+    response_data = response.data.decode('utf-8')
+    assert '<h1 class="mb-5">Edit Movie</h1>' in response_data
+    assert type(movie) == Movie
--- tests/unit/test_update_movie.py
+++ tests/unit/test_update_movie.py
@@ -1,1 +1,17 @@
-# TODO: Feature 5
+from src.models.movie import Movie
+
+
+def test_update_movie():
+    # Setup: Create a repository and add a test movie
+  def test_movie_model():
+    movie = Movie(123, 'Star Wars', 'George Lucas', 5)
+    # Update the movie
+    updated_title = "Updated Star Wars"
+    updated_director = "Updated George Lucas"
+    updated_rating = 3
+    movie.update_movie(movie.id, updated_title, updated_director, updated_rating)
+    
+    assert movie.movie_id == 123
+    assert updated_title.title == 'Updated Star Wars'
+    assert updated_director.director == 'Updated George Lucas'
+    assert updated_rating.rating == 3
