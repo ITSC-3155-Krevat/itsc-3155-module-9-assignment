bdf6d44450404407e033e45994962121c5eb08c8
Ronni Elhadidy <eronni1@gmail.com>
2024-03-20 15:27:27 -0400

    tests complete

--- tests/e2e/test_edit_movie_page.py
+++ tests/e2e/test_edit_movie_page.py
@@ -1,12 +1,16 @@
 # TODO: Feature 5
 from flask.testing import FlaskClient
-from src.models.movie import Movie
+from app import app
+from src.repositories.movie_repository import get_movie_repository
 
-
+movie_repository = get_movie_repository()
 
 def test_edit_page(test_app: FlaskClient):
-    movie = Movie(123, 'Star Wars', 'George Lucas', 5)
-    response = test_app.get(f"/movies/{movie.movie_id}/edit")  
+    movie_repository.create_movie('Spiderman', 'Ronni Elhadidy', 5)
+    movie = movie_repository.get_movie_by_title('Spiderman')
+    movie_id = movie.movie_id
+
+    response = test_app.get(f'/movies/{movie_id}/edit')
     response_data = response.data.decode('utf-8')
-    assert '<h1 class="mb-5">Edit Movie</h1>' in response_data
-    assert type(movie) == Movie
+    assert response.status_code == 200
+    assert f'<button type="submit" class="btn btn-primary">Update Movie</button>' in response_data
