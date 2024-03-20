9811ce6e6495e5e24ebce6a56c0dc299dc4ea721
kamanna12 <kamanna@uncc.edu>
2024-03-20 15:36:54 -0400

    updated test

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -8,3 +8,8 @@ def test_create_movie_end_to_end(test_app: FlaskClient):
     response_data = response.data.decode("utf-8")
     assert response.status_code == 200
     assert '<h1 class="mb-5">Create Movie Rating</h1>' in response_data
+    assert '<label for="title" class="form-label">Title:</label>' in response_data
+    assert '<label for="director" class="form-label">Director:</label>' in response_data
+    assert '<label for="rating" class="form-label">Rating:</label>' in response_data
+    assert '<button type="submit" class="btn btn-primary">Create Movie</button>' in response_data
+
