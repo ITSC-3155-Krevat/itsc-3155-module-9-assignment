01a82af1c83ccb8f6290bb46623e460b9e6556d0
Hannah_Zeru <hannahzeru50@gmail.com>
2024-03-20 22:48:01 -0400

    losing my mind, but feature 4 is done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 # Get the movie repository singleton to use throughout the application
 movie_repository = get_movie_repository()
-
 
 @app.get('/')
 def index():
@@ -40,7 +39,13 @@ def search_movies():
 @app.get('/movies/<int:movie_id>')
 def get_single_movie(movie_id: int):
     # TODO: Feature 4
-    return render_template('get_single_movie.html')
+    #call id
+    movie = movie_repository.get_movie_by_id(movie_id)
+    if movie == 'None':
+        return 'Movie not found.'
+    else:
+        redirect(f'/movies/{movie_id}')
+        return render_template('get_single_movie.html', movie=movie)
 
 
 @app.get('/movies/<int:movie_id>/edit')
