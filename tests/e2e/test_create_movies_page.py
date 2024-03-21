868018740b6ce1b72462e91477665375c2fe30d7
Sam-Is-Red <157077824+Sam-Is-Red@users.noreply.github.com>
2024-03-20 20:14:36 -0400

    Sam's commit of Sams-Branch-1 (#4)
    Co-authored-by: Samuel <ssanto6@gmail.com>

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,1 +1,14 @@
 # TODO: Feature 2
+
+from app import app
+
+
+
+def test_create_movie():
+    test_app = app.test_client()
+    
+    response = test_app.get('/create_movie')
+    
+    assert response.status_code == 200
+    assert <p class="mb-3">Create a movie rating below</p> in response.data
+
--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,10 @@
 # TODO: Feature 2
+
+from app import create_movie
+
+def test_create_movie():
+    movie = create_movie('Star Wars', 'George Lucas', 5)
+
+    assert movie.title == 'Star Wars'
+    assert movie.director == 'George Lucas'
+    assert movie.rating == 5
