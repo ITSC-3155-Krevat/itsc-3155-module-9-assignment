0d54980ed352ddf6666c94a5ab99f082ae7bedc6
Betty Bangali <ebangali@uncc.edu>
2024-03-20 23:20:41 -0400

    this final submission

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, abort
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -28,14 +29,17 @@ def create_movies_form():
 def create_movie():
     # TODO: Feature 2
     # After creating the movie in the database, we redirect to the list all movies page
-    return redirect('/movies')
+        return redirect('/movies')
 
 
 @app.get('/movies/search')
 def search_movies():
     # TODO: Feature 3
-    return render_template('search_movies.html', search_active=True)
-
+    movie_title = request.form.get('title')
+    movie_rating = movie_repository.get_movie_by_title(movie_title)
+    if movie_rating is None:
+        print(f"Movie not found. Please try another movie")
+    return render_template('search_movies.html',movie_rating=movie_rating)
 
 @app.get('/movies/<int:movie_id>')
 def get_single_movie(movie_id: int):
